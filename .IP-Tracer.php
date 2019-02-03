68394b8e3715b3024b7c05f30914680c5f091269
oscaroasis <47290487+oscaroasis@users.noreply.github.com>
2019-02-04 00:32:34 +0700

    Update .IP-Tracer.php

--- .IP-Tracer.php
+++ .IP-Tracer.php
@@ -29,7 +29,7 @@ function About() {
   echo <<<EOL
 
          \033[01;33mTool Name \033[01;37m:- \033[01;36mIP-Tracer
-         \033[01;33mAuthor \033[01;37m:- \033[01;36mRajkumar Dusad
+         \033[01;33mAuthor \033[01;37m:- \033[01;36mØSC4R O’4S1S
          \033[01;33mPowered By \033[01;37m:- \033[01;36mAex Software's
 
  \033[01;33mIP-Tracer\033[01;32m is use \033[01;36mip-api \033[01;32mto retrive any ip address information. Our system will automatically ban any IP addresses doing over 150 requests per minute.\033[00m
@@ -44,8 +44,8 @@ function upd() {
   logo();
   echo "\n\033[01;32mUpdating IP-Tracer.........\033[01;37m\n\n";
   sleep(1);
-  system("cd ~/ && git clone https://github.com/Rajkumrdusad/IP-Tracer.git");
-  system("cd ~/ && sudo git clone https://github.com/Rajkumrdusad/IP-Tracer.git");
+  system("cd ~/ && git clone https://github.com/oscar-oasis/IPTRACER.git");
+  system("cd ~/ && sudo git clone https://github.com/oscar-oasis/IPTRACER.git");
   system("cd ~/IP-Tracer && sh install");
   logo();
   echo "\n\033[01;32m              IP-Tracer updated !!!\033[01;37m\n";
@@ -63,9 +63,9 @@ function menu() {
   echo "   \033[01;32m[ \033[01;37mx \033[01;32m] \033[01;33mExit \n\n\033[00m";
   $inp = readline('  IP-Tracer >> ');
   if ($inp=="x" || $inp=="exit") {
-    echo "\n\033[01;31m  Exiting .......\033[00m\n";
+    echo "\n\033[01;31m  Exit\033[00m\n";
     sleep(1);
-    echo "\033[01;32m  By ....... :)\n\n\033[00m";
+    echo "\033[01;32m  Bye\n\n\033[00m";
     exit();
   } else if ($inp=="1") {
     trac();
